c0b50851454c82235c8ecf38055f85d5cd02c585
Vinay Valsan <vinayvvv444@gmail.com>
2020-08-28 17:44:40 +0400

    name changed for test run

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -22,7 +22,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Les Jackson";
+            return "Les Jackso";
         }
 
         // POST api/values
