35e7befb00e9b5947b489a3afa24044490bd30bf
Vinay Valsan <vinayvvv444@gmail.com>
2020-08-28 21:49:23 +0400

    changed return values

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "playbook" };
+            return new string[] { "dotnet", "vinay" };
         }
 
         // GET api/values/5
