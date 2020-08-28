9f4b19a8ec2d58003432371074e2f046f963f357
Vinay Valsan <vinayvvv444@gmail.com>
2020-08-28 17:11:39 +0400

    first commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using SimpleAPI.Controllers;
+using System;
+using Xunit;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Les Jackson", returnValue.Value);
+        }
+
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
